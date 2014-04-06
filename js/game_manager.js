577701bb5ae282829e47ffe6f84eb8308d1048f7
Benjamin Englard <benjienglard@yahoo.com>
2014-04-05 23:05:10 -0400

    Fibonacci

--- js/game_manager.js
+++ js/game_manager.js
@@ -5,6 +5,7 @@ function GameManager(size, InputManager, Actuator, StorageManager) {
   this.actuator       = new Actuator;
 
   this.startTiles     = 2;
+  this.fib = [1,1,2,3,5,8,13,21,34,55,89,144,233,377,610,987];
 
   this.inputManager.on("move", this.move.bind(this));
   this.inputManager.on("restart", this.restart.bind(this));
@@ -72,7 +73,7 @@ GameManager.prototype.addStartTiles = function () {
 // Adds a tile in a random position
 GameManager.prototype.addRandomTile = function () {
   if (this.grid.cellsAvailable()) {
-    var value = Math.random() < 0.9 ? 2 : 4;
+    var value = Math.random() < 0.9 ? 1 : 2;
     var tile = new Tile(this.grid.randomAvailableCell(), value);
 
     this.grid.insertTile(tile);
@@ -143,6 +144,8 @@ GameManager.prototype.move = function (direction) {
   var traversals = this.buildTraversals(vector);
   var moved      = false;
 
+
+
   // Save the current tile positions and remove merger information
   this.prepareTiles();
 
@@ -157,8 +160,13 @@ GameManager.prototype.move = function (direction) {
         var next      = self.grid.cellContent(positions.next);
 
         // Only one merger per row traversal?
-        if (next && next.value === tile.value && !next.mergedFrom) {
-          var merged = new Tile(positions.next, tile.value * 2);
+
+        if (next && 
+            (self.fib.indexOf(next.value) != -1) &&
+            (self.fib.indexOf(tile.value) != -1) &&
+            (self.fib.indexOf(next.value + tile.value) != -1) &&
+            !next.mergedFrom) {
+          var merged = new Tile(positions.next, tile.value + next.value);
           merged.mergedFrom = [tile, next];
 
           self.grid.insertTile(merged);
@@ -171,7 +179,7 @@ GameManager.prototype.move = function (direction) {
           self.score += merged.value;
 
           // The mighty 2048 tile
-          if (merged.value === 2048) self.won = true;
+          if (merged.value === 987) self.won = true;
         } else {
           self.moveTile(tile, positions.farthest);
         }
@@ -260,7 +268,10 @@ GameManager.prototype.tileMatchesAvailable = function () {
 
           var other  = self.grid.cellContent(cell);
 
-          if (other && other.value === tile.value) {
+          if (other && 
+            (self.fib.indexOf(tile.value) != -1) &&
+            (self.fib.indexOf(other.value) != -1) &&
+            (self.fib.indexOf(other.value + tile.value) != -1)) {
             return true; // These two tiles can be merged
           }
         }
